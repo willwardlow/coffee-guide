8b19c1ffe4d13a30460d99aa65029f7a28ad9949
willwardlow <wardlow.william@gmail.com>
2021-02-19 10:25:20 -0600

    Everything works!!!!!!!

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -2,6 +2,6 @@ export const baseURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRT
 
 export const config = {
   headers: {
-    Authorization: `Bearer: ${process.env.REACT_APP_AIRTABLE_KEY}`,
+    Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
   },
 };
